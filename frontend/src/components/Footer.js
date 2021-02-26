b18dd983eca68dbaa9d7a4fad7e51de1ad7cc1d2
MrSergeyB <blokhin_sergey@hotmail.com>
2021-02-26 23:38:15 +0900

    desided to go with bootstrap

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>Designed by Kei Blokhin</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
